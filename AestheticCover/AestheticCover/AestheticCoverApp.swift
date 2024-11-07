546fbf15cf501ad1670947a69d3cbfd4ec586a65
chys.charlie <chys.charlie@bytedance.com>
2024-11-07 13:53:25 +0800

    add .gitignore

---
+++ AestheticCover/AestheticCover/AestheticCoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  AestheticCoverApp.swift
+//  AestheticCover
+//
+//  Created by ByteDance on 2024/11/6.
+//
+
+import SwiftUI
+
+@main
+struct AestheticCoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AestheticCover/AestheticCover/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AestheticCover
+//
+//  Created by ByteDance on 2024/11/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
